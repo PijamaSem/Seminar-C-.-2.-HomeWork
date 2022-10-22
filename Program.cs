78399fdb7b36410e133735476c40f3091b730f14
Pijama_sem <example@example.com>
2022-10-22 13:48:02 +0300

    second task

--- Program.cs
+++ Program.cs
@@ -11,3 +11,18 @@ Console.WriteLine("Pls enter numerics over 99 and less 1000");
 int a = Convert.ToInt32(Console.ReadLine());
 int result = SecondDigit(a);
 Console.WriteLine($"Second digit of numeris is {result}");*/
+
+// Task #13 Write program witch out 3rd digit of input numeric or test "Numeric haven't 3rd digit
+/*
+int ThirdDigit(int num){
+    int digMore3Digit = num / 100;
+    int dig3 =  digMore3Digit % 10;
+    return dig3;
+}
+Console.WriteLine("Pls enter numerics");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a<100){
+    Console.WriteLine($"nummeric less 100");
+}else{
+    Console.WriteLine($"Third digit of numeric is {ThirdDigit(a)}");
+}*/
