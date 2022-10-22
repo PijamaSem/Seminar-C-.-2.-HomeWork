f9c7d583b0144fe6587d2defe80943613c945c3e
Pijama_sem <example@example.com>
2022-10-22 13:30:47 +0300

    first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Seminar #2. Home work. Alexeev Andrey.
+
+// Task #10. Write program witch take 2 numerics and output 2nd digit
+/*
+int SecondDigit(int num){
+    int dig12 = num / 10;
+    int dig2 =  dig12 % 10;
+    return dig2;
+}
+Console.WriteLine("Pls enter numerics over 99 and less 1000");
+int a = Convert.ToInt32(Console.ReadLine());
+int result = SecondDigit(a);
+Console.WriteLine($"Second digit of numeris is {result}");*/
